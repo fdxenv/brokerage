b2aea8456aa721a167b5d8b963c5369c31a613ce
Sri <sri@gmail.com>
2018-12-25 22:16:33 +0000

    first commit

---
+++ src/main/java/com/fdx/App.java
@@ -0,0 +1,13 @@
+package com.fdx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
